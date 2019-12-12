a2ecaddcca6c5d43ce4f7e190161c3f9ac0eb43a
David Zhang <zdavid0123@gmail.com>
2019-12-12 09:20:11 -0500

    force yes

--- run.sh
+++ run.sh
@@ -2,11 +2,10 @@
 set -e
 
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 sudo apt-cache policy docker-ce
-sudo apt install docker-ce
-sudo systemctl status docker
+sudo apt -y install docker-ce
 sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)"
